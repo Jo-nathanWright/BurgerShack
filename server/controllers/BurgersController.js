9c67881ddcccd9ec968317069a511d46d7141f74
Jonathan Wright <j.wright72127@gmail.com>
2021-07-26 14:31:50 -0600

    Finished Up most functionality

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -6,13 +6,53 @@ export class BurgersController extends BaseController {
     super('api/burgers')
     this.router
       .get('', this.getAll)
-      // .post('', this.create)
+      .get('/:id', this.getById)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
   }
 
   getAll(req, res, next) {
     try {
       const burgers = burgersService.getAll()
       res.send(burgers)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  getById(req, res, next) {
+    try {
+      const burger = burgersService.getById(req.params.id)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  create(req, res, next) {
+    try {
+      const burger = burgersService.create(req.body)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const burger = burgersService.edit(req.body)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  delete(req, res, next) {
+    try {
+      burgersService.delete(req.params.id)
+      res.send('Off the menu!')
     } catch (error) {
       next(error)
     }
